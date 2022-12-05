6f75e79cb2dc8013a1cb809efd657606d430ada9
Babajide Akinseye <bakinseye247@gmail.com>
2022-12-05 10:28:04 +0100

    add serve file endpoint

--- server.js
+++ server.js
@@ -1,25 +1,59 @@
 const express = require("express");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const crypto = require("crypto");
 const cors = require("cors");
+const path = require("path");
+const fs = require("fs");
+
+let finalFileName;
+let originalFileName;
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, path.join(__dirname, "./uploads"));
+    },
+    filename: (req, file, cb) => {
+      const customFileName = crypto.randomBytes(18).toString("hex");
+      const fileExtension = path.extname(file.originalname);
+
+      finalFileName = customFileName + fileExtension;
+      originalFileName = file.originalname;
+      // check extension and reject
+      req.uploadedFileName = finalFileName;
+      cb(null, finalFileName);
+    },
+  }),
+});
 
 const app = express();
 const port = process.env.PORT || 4000;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({}));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use(cors({}));
+app.get("/uploads/:filename", (req, res) => {
+  const filename = req.params.filename;
+  // Code to serve file here
+  return res.sendFile(path.join(__dirname, "./uploads", filename));
+});
 
 app.post("/upload_files", upload.single("podcast"), uploadFiles);
 
 function uploadFiles(req, res) {
-  console.log(req.body);
+  // console.log(req.body);
+  console.log(finalFileName);
   //   console.log(req.file);
-  res.json({ message: "Successfully uploaded files" });
+  res.json({
+    success: true,
+    originalFileName,
+    uploadedFile: finalFileName,
+  });
 }
 app.listen(port, () => {
   console.log(`Server started...`);
